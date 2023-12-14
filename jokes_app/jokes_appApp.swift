dc3bbae3f629fa7a935e7845a9d684f83786088a
Vilius Bundulas <vilius.bundulas@gmail.com>
2023-12-14 13:11:42 +0200

    Initial Commit

---
+++ jokes_app/jokes_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  jokes_appApp.swift
+//  jokes_app
+//
+//  Created by Vilius Bundulas on 14/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct jokes_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
