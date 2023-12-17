32d7d976a709ffbb83c1581385fd982d8edee35d
Vilius Bundulas <vilius.bundulas@gmail.com>
2023-12-17 22:38:56 +0200

    wip

--- jokes_app/Views/HomeView.swift
+++ jokes_app/Views/HomeView.swift
@@ -13,23 +13,26 @@ struct HomeView: View {
     @EnvironmentObject var jokesViewModel: JokesViewModel
     
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-            
-            Text(jokesViewModel.dadJoke?.joke ?? "There is no dad joke")
-            Text(jokesViewModel.chuckNorrisJokes?.value ?? "There is no chuck norris joke")
-            
-            Button("Fetch Chuck Norris Jokes") {
-                jokesViewModel.getJoke(from: .chuckNorrisJokes)
+        NavigationStack {
+            VStack {
+                
+                Text(jokesViewModel.dadJoke?.joke ?? "There is no dad joke")
+                Text(jokesViewModel.chuckNorrisJokes?.value ?? "There is no chuck norris joke")
+                
+                JokesButton(title: "Get random Dad joke") {
+                    jokesViewModel.getJoke(from: .dadJokes)
+                }
+                
+                Text("Or")
+                    .font(.caption)
+                    .foregroundStyle(.black.opacity(0.8))
+                    .padding(.vertical, 5)
+                
+                JokesButton(title: "Get random Chuck Norris joke") {
+                    jokesViewModel.getJoke(from: .dadJokes)
+                }
             }
-            
-            Button("Fetch Dad Jokes") {
-                jokesViewModel.getJoke(from: .dadJokes)
-            }
+            .padding()
         }
-        .padding()
     }
 }
